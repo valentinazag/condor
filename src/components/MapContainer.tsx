ec6bfdb4a7e1348f9fca6d6fad599bc135b7701f
VZagman_KPT <VZagman@kopiustech.com>
2026-06-13 14:30:10 +0000

    separate hook logic and add styles to markers

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,24 +1,9 @@
 import maplibregl from 'maplibre-gl';
 import { useEffect, useRef } from 'react';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import * as v from 'valibot';
 import '../App.css';
+import { useEarthquakes } from '../hooks/useEarthquakes';
 import type { FilterParams } from '../types/FilterParams';
-
-const EarthquakeSchema = v.object({
-	features: v.array(
-		v.object({
-			properties: v.object({
-				mag: v.nullable(v.number()),
-				place: v.nullable(v.string()),
-				time: v.number(),
-			}),
-			geometry: v.object({
-				coordinates: v.tuple([v.number(), v.number(), v.number()]),
-			}),
-		}),
-	),
-});
 
 type MapContainerProps = {
 	filters: FilterParams | null;
@@ -26,43 +11,71 @@ type MapContainerProps = {
 
 export function MapContainer({ filters }: MapContainerProps) {
 	const mapContainer = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		if (!filters) return;
-		const { starttime, endtime, minmagnitude } = filters;
-		const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${starttime}&endtime=${endtime}&minmagnitude=${minmagnitude}`;
-
-		async function fetchEarthquakes() {
-			const response = await fetch(url);
-			const data = await response.json();
-			const parsedData = v.parse(EarthquakeSchema, data);
-			console.log(
-				'earthquakes:',
-				parsedData.features.map((f) => ({
-					...f.properties,
-					date: new Date(f.properties.time).toISOString(),
-					latitude: f.geometry.coordinates[1],
-					longitude: f.geometry.coordinates[0],
-					depth: f.geometry.coordinates[2],
-				})),
-			);
-		}
-
-		fetchEarthquakes();
-	}, [filters]);
-
+	const mapRef = useRef<maplibregl.Map | null>(null);
+	const { earthquakes, loading, error } = useEarthquakes(filters);
 	useEffect(() => {
 		if (!mapContainer.current) return;
 
-		const map = new maplibregl.Map({
+		mapRef.current = new maplibregl.Map({
 			container: mapContainer.current,
 			style: 'https://tiles.openfreemap.org/styles/liberty',
 			center: [0, 0],
-			zoom: 2,
 		});
-
-		return () => map.remove();
+		return () => mapRef.current?.remove();
 	}, []);
 
-	return <div ref={mapContainer} className="map-style" />;
+	useEffect(() => {
+		if (!mapRef.current || earthquakes.length === 0) return;
+
+		earthquakes.forEach((feature) => {
+			const [longitude, latitude] = feature.geometry.coordinates;
+			const { place, mag: magnitude, time } = feature.properties;
+			let color = '#a0aec0';
+			let size = 15;
+			if (magnitude !== null) {
+				size = Math.max(15, magnitude * 6);
+
+				if (magnitude < 3) {
+					color = '#22c55e';
+				} else if (magnitude < 5) {
+					color = '#eab308';
+				} else if (magnitude < 7) {
+					color = '#f97316';
+				} else {
+					color = '#ef4444';
+				}
+			}
+
+			const el = document.createElement('div');
+			el.className = 'custom-marker';
+			el.style.backgroundColor = color;
+			el.style.width = `${size}px`;
+			el.style.height = `${size}px`;
+
+			const formattedTime = time
+				? new Date(time).toLocaleString()
+				: 'Fecha no disponible';
+			const popupContent = `
+            <div style="font-family: sans-serif; padding: 5px;">
+                <h3 style="margin: 0 0 5px 0; color: ${color};">Magnitud: ${magnitude ?? 'N/A'}</h3>
+                <p style="margin: 0 0 5px 0; font-weight: bold;">${place ?? 'Lugar no disponible'}</p>
+                <small style="color: #666;">${formattedTime}</small>
+            </div>
+        `;
+
+			new maplibregl.Marker({ element: el })
+				.setLngLat([longitude, latitude])
+				.setPopup(
+					new maplibregl.Popup({ offset: size / 2 }).setHTML(popupContent),
+				)
+				.addTo(mapRef.current!);
+		});
+	}, [earthquakes]);
+	return (
+		<>
+			{loading && <p>Loading...</p>}
+			{error && <p>Error: {error}</p>}
+			<div ref={mapContainer} className="map-style" />
+		</>
+	);
 }
